e5d62c6a1ad870707884c22e962dd4a9920c3abf
RitikaAg <ritikaagarwal646@gmail.com>
2018-04-15 23:41:52 +0530

    commit

--- app/src/main/java/com/example/android/complaintapp/SuccessActivity.java
+++ app/src/main/java/com/example/android/complaintapp/SuccessActivity.java
@@ -2,7 +2,6 @@ package com.example.android.complaintapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 /**
  * Created by ritika on 4/3/18.
@@ -17,9 +16,9 @@ public class SuccessActivity extends FormcomplaintActivity  {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_success);
-        TextView keyview=(TextView) findViewById(R.id.key);
+       /* TextView keyview=(TextView) findViewById(R.id.key);
         //String key=getIntent().getStringExtra("fkey");
-        keyview.setText("Your Complaint Has Been Registered Successfully"+"\n"+"Further Details Shall Be Mailed To You");
+        keyview.setText("Your Complaint Has Been Registered Successfully"+"\n"+"Further Details Shall Be Mailed To You");*/
         Thread thread=new Thread()
         {
             @Override
